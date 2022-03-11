d524c13bdfecd504271034981d90cd44cf0f94b6
Heather Brooke <heather.brooke@rainbowarray.com>
2022-03-11 15:02:16 -0600

    Set max data items per level to 4 for now to ensure it compiles in most circumstances.

--- .eleventy.js
+++ .eleventy.js
@@ -21,7 +21,7 @@ const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 module.exports = config => {
   let
-    maxDataItemsPerLevel = 7,
+    maxDataItemsPerLevel = 4,
     cacheDuration = '1s',
     cacheDirectory = '.cache',
     cacheDurationServerless = '*',
